7e2231fd1f2562ea698b6eafe5380da9e98126c6
Moritz <m.dwulit@gmx.at>
2026-04-20 10:30:18 +0200

    Tested Move gen with some other Positions.

--- src/main.cpp
+++ src/main.cpp
@@ -71,8 +71,8 @@ int main() {
     Zobrist::initKeys();
     Attacks::init();
 
-    board.parseFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+    board.parseFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 
-    perftDivide(5);
+    perftDivide(4);
     return 0;
 }
